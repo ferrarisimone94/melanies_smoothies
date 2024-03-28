f093cf82fe7e55567363c5fb26845b5bf0ac0567
ferrarisimone94 <77260007+ferrarisimone94@users.noreply.github.com>
2024-03-28 17:35:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,11 @@ st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write(
-    """Choose the fruits you want in your cutom Smoothie!"""
+    """Welcome! Choose the fruits you want in your cutom Smoothie!"""
 )
 
-#conn = st.experimental_connection()
 conn = st.experimental_connection("snowpark") # Config section defined in [connections.sql] in secrets.toml.
-#session = conn.session()
 my_dataframe = conn.session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 #convert the snowpark df to a Pandas df so we can use LOC function
 pd_df = my_dataframe.to_pandas()
@@ -27,20 +23,17 @@ st.dataframe(pd_df)
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     ,my_dataframe
-    ,max_selections = 5
+    ,max_selections = 6
 )
 
 #if the list is not empty do....
 if ingredients_list: 
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
     
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(search_on + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=False)
@@ -50,8 +43,8 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit Order')
     
-    #st.write(my_insert_stmt)
-    #st.stop()
     if time_to_insert:
         conn.session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+        
+
