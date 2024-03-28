c3b4a4bd4df7a7c5db6666a37f0ffdbad871971b
ferrarisimone94 <77260007+ferrarisimone94@users.noreply.github.com>
2024-03-28 12:01:09 -0400

    Update streamlit_app.py
    Connected changes needed to move from SIS to SNIS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session COMMENTED TO PASS FROM SIS TO SNIS
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -17,8 +17,9 @@ st.write(
 #                      ('Banana', 'Strawberries', 'Peaches'))
 #st.write('You selected:', option)
 
-
-session = get_active_session()
+cnx = st.connection("snowflakes") 
+session = cnx.session()
+#session = get_active_session() COMMENTED TO PASS FROM SIS TO SNIS
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
