46d0be869a3fe863ae9b19fe3120f37ded6c64ac
ferrarisimone94 <77260007+ferrarisimone94@users.noreply.github.com>
2024-04-02 11:24:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,38 +9,9 @@ from snowflake.snowpark.functions import col
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 
 name_on_order = st.text_input('Name on Smoothie:')
-#st.write(
-#    """Here the list of the ingredients available!"""
-#)
 
 conn = st.experimental_connection("snowpark")
 my_dataframe = conn.session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-
-#total_ingredient = ""
-#number = 0
-#ingredients_ordered_df = conn.session.table("SMOOTHIES.PUBLIC.ORDERS").select(col('ingredients')).collect()
-#for i in ingredients_ordered_df:
-#    total_ingredient = total_ingredient + str(i)
-
-#total_ingredient = total_ingredient.replace("Row(INGREDIENTS=","(")
-#total_ingredient = total_ingredient.replace(", ","'),('")
-#total_ingredient = total_ingredient.replace("('')","")
-
-#def Convert(string): 
-#    li = list(string.split(", ")) 
-#    return li
-
-#if total_ingredient[len(total_ingredient)-1] == ',':
-#    total_ingredient = total_ingredient[:-1]
-
-#add_ingredients = """ insert into smoothies.public.ing_ordered(ingredients_ordered)
-#            values """ + total_ingredient + """;"""
-
-#try: 
-#    conn.session.sql("truncate table smoothies.public.ing_ordered;").collect()
-#    conn.session.sql(add_ingredients).collect()
-#except: 
-#    st.write('Order data not uploaded')
 
 #convert the snowpark df to a Pandas df so we can use LOC function
 pd_df = my_dataframe.to_pandas()
@@ -52,13 +23,7 @@ ingredients_list = st.multiselect(
     ,max_selections = 6
 )
 
-#created_dataframe = conn.session.sql("select INGREDIENTS_ORDERED as Ingredients, count(INGREDIENTS_ORDERED) as count from ing_ordered group by INGREDIENTS_ORDERED order by count desc;")
-
-#queried_data = created_dataframe.to_pandas()
-
-st.subheader("Cannot decide?")
-st.subheader("Look at what our customer love!")
-#st.bar_chart(data=queried_data, x="INGREDIENTS", y="COUNT" )
+st.subheader("Cannot decide? Look at what our customer love in the page on the left!")
 
 #if the list is not empty do....
 if ingredients_list: 
